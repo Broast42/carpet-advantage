86c4dd59c26202ba3d8ddd237ce538df1870806f
rdowd <rjarrettdowd@yahoo.com>
2020-09-07 12:02:38 -0400

    Added footer

---
+++ client/src/data.js
@@ -0,0 +1,11 @@
+const links = [
+    {name: "HOME", link: '#'},
+    {name: "SERVICES", link: '#'},
+    {name: "BEFORE AND AFTER", link: '#'},
+    {name: "GO CAMPING", link: '#'},
+    {name: "ABOUT US", link:"#"}
+]
+
+export {
+    links,
+}
