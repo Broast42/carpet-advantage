754cccec81849505dd962933a61496eb5755fc2f
rdowd <rjarrettdowd@yahoo.com>
2020-09-11 22:56:42 -0400

    Added elctrostatic section

---
+++ client/src/components/home/Electro.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Electro() {
+  return (
+    <section>
+        <div className="electro-box">
+            <div className="new-title-box">
+                <div className="new-text">
+                    NEW!
+                </div>
+                
+            </div>
+            <div className="electro-title">
+                <h3>Electrostatic Disinfecting</h3>
+            </div>
+        </div>
+    </section>
+    
+  );
+}
+
+export default Electro;
