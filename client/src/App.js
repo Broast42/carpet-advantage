e78890e3851f7e753f55e4ed84aff1ecbf282156
rdowd <rjarrettdowd@yahoo.com>
2020-11-11 14:52:39 -0500

    Added switch and home route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/pages/home/Home'
-//import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import './styles/styles.scss'
 
 function App() {
   return (
     <div>
       <Header/>
-
-      <Home/>
-
+      
+      <Switch>
+        
+        <Route path="/" >
+          <Home/>
+        </Route>
+      
+      </Switch>
+    
       <Footer/>
     </div>
     
